7e81fd0344d1d6acb0a075a69db368ac954ae929
shadowmonarchgin <tomarugin123@gmail.com>
2022-07-31 13:31:42 +0800

    Luminessence Tweak
    changed the recipes

---
+++ modpack/overrides/kubejs/server_scripts/luminessence.js
@@ -0,0 +1,17 @@
+onEvent('recipes', event => {
+
+ event.recipes.createMixing('2x extendedcrafting:luminessence', [
+        'minecraft:redstone_block',
+		'minecraft:redstone_block',
+		'minecraft:redstone_block',
+        'minecraft:glowstone',
+		'minecraft:glowstone',
+		'minecraft:glowstone',
+		'thermal:gunpowder_block',
+		'thermal:gunpowder_block',
+		'thermal:gunpowder_block',
+		'bloodmagic:reagentbinding',
+		'hexcasting:dye_colorizer_yellow'
+      ]).heated()
+event.remove({output: 'extendedcrafting:luminessence'})
+})
