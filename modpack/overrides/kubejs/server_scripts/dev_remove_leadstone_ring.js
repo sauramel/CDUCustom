e63fe80b4645842737ef1a2c7a2cc5119ba9492d
shadowmonarchgin <tomarugin123@gmail.com>
2022-10-06 13:11:54 +0800

    Changes
    3.0

--- modpack/overrides/kubejs/server_scripts/dev_remove_leadstone_ring.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-  event.remove({output: 'angelring:leadstone_angel_ring', mod: 'angelring'})
-})
--- modpack/overrides/kubejs/server_scripts/dev_remove_reinforced_ring.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-  event.remove({output: 'angelring:resonant_angel_ring', mod: 'angelring'})
-})
