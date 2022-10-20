6913796b3a33a61b29ff32e241833aef307d4398
Loufoque <cozypeep09@gmail.com>
2022-10-20 18:04:42 +0800

    Revert to 2.3.0

---
+++ modpack/overrides/kubejs/server_scripts/angel_ring.js
@@ -0,0 +1,35 @@
+onEvent('recipes', event => {
+    event.custom({
+
+            "type": "extendedcrafting:shaped_table",
+            "pattern": [
+              "ABBBBBBBA",
+              "BACDEDCAB",
+              "BCAFGFACB",
+              "BDFHHHFDB",
+              "BEIHJHIEB",
+              "BDFHHHFDB",
+              "BCAFKFACB",
+              "BACDEDCAB",
+              "ABBBBBBBA"
+            ],
+            "key": {
+              "A": "enigmaticlegacy:evil_essence" ,
+              "B": "forbidden_arcanus:golden_dragon_scale",
+              "C": "extendedcrafting:crystaltine_block",
+              "D": "ars_nouveau:wilden_tribute",
+              "E": "stalwart_dungeons:void_crystal",
+              "F": "mythicbotany:alfsteel_block",
+              "G": "enigmaticlegacy:fabulous_scroll",
+              "H": "forbidden_arcanus:dark_nether_star",
+              "I": "enigmaticlegacy:evil_ingot",
+              "J": "angelring:itemdiamondring",
+              "K": "buddycards:buddycard_end26",
+            },
+            "result": {
+              "item": "angelring:itemring"
+            }
+
+    })
+    event.remove({output: 'angelring:itemring', mod: 'angelring'})
+})
