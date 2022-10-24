4faa4242970066e26a3547d24e872eb6eee93db5
Loufoque <cozypeep09@gmail.com>
2022-10-24 16:59:32 +0800

    Revert to 3.0.1

--- modpack/overrides/kubejs/server_scripts/dev_remove_hardened_ring.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-  event.remove({output: 'angelring:hardened_angel_ring', mod: 'angelring'})
-})
--- modpack/overrides/kubejs/server_scripts/dev_remove_resonant_ring.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-  event.remove({output: 'angelring:reinforced_angel_ring', mod: 'angelring'})
-})
